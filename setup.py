0e117728708e9ef2274fffef1427f8da53d7f4b2
Cristian-Napitupulu <13319004@mahasiswa.itb.ac.id>
2024-06-04 01:54:17 +0700

    Add setup.py for easier setup

---
+++ setup.py
@@ -0,0 +1,7 @@
+# Description: This file is used to setup the project and run the necessary scripts to train the models and create the charts.
+
+import data.olah_intents
+import data.create_chart
+
+import train_bert
+import train_neural_network
